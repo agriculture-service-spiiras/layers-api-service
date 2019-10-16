218ff91a5e167e3b9c9d6b8ae6765715edcdd700
Evgeny Karasev <gnomskg@gmail.com>
2019-10-16 17:17:26 +0300

    add base routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+// Get layers list
 router.get("/", function(req, res, next) {
-  res.sendStatus(200);
+  res.send("Send layers list");
+});
+
+// Get any layer by id
+router.get("/:id", function(req, res, next) {
+  res.send("Send any layer by id");
+});
+
+router.post("/", function(req, res, next) {
+  res.send("Send result of posting new layer");
+});
+
+router.put("/:id", function(req, res, next) {
+  res.send("Set result of updating any layer by id");
 });
 
 module.exports = router;
