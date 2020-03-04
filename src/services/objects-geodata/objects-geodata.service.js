3ff5f3584d5f846bfdf03c0da3f7cf12ee8ee32b
Evgeny Karasev <gnomskg@gmail.com>
2020-03-04 17:52:11 +0300

    feat: add base implementation of objects-geodata service

---
+++ src/services/objects-geodata/objects-geodata.service.js
@@ -0,0 +1,17 @@
+// Initializes the `objects-geodata` service on path `/objects-geodata`
+const { ObjectsGeodata } = require('./objects-geodata.class');
+const hooks = require('./objects-geodata.hooks');
+
+module.exports = function(app) {
+  const options = {
+    paginate: app.get('paginate'),
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/objects-geodata', new ObjectsGeodata(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('objects-geodata');
+
+  service.hooks(hooks);
+};
